799be3971a8684807d25a5567544e2b264d77643
Brayan Jenzy La Cruz Carreño <blacruzc@emeal.nttdata.com>
2022-04-05 17:32:01 -0500

    TLS 1.2 para conexion a mongoatlas

--- src/main/java/com/nttdata/customerservice/CustomerServiceApplication.java
+++ src/main/java/com/nttdata/customerservice/CustomerServiceApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CustomerServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    System.setProperty("jdk.tls.client.protocols", "TLSv1.2"); // esto debido a que la version actual de JDK 11.02 ejecuta TLS < 1.2
+    SpringApplication.run(CustomerServiceApplication.class, args);
+  }
 
 }
